06b531af72e943237b7d2fd70435483ae4e19ad4
Ubuntu <ubuntu@ip-172-31-33-254.ec2.internal>
2024-11-05 15:02:47 +0000

    add backend

--- main.tf
+++ main.tf
@@ -5,6 +5,11 @@ terraform {
       version = "5.74"
     }
   }
+  backend "s3" {
+    bucket = "tf-state-lab-florent"
+    key    = "tf-state"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
@@ -17,3 +22,13 @@ resource "aws_vpc" "vpc" {
     Name = "main-tf"
   }
 }
+
+resource "aws_s3_bucket" "tf-state" {
+  bucket = "tf-state-lab-florent"
+
+  tags = {
+    Name        = "Terraform remote state"
+    Environment = "Dev"
+  }
+}
+
