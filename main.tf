0ad5d088337e98782ed2263623b12945440c5f13
Florent <florent.houbart@gmail.com>
2024-11-05 16:22:44 +0100

    ressources

--- main.tf
+++ main.tf
@@ -16,19 +16,34 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "main-tf"
-  }
-}
-
 resource "aws_s3_bucket" "tf-state" {
   bucket = "tf-state-lab-florent"
 
   tags = {
     Name        = "Terraform remote state"
     Environment = "Dev"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-06b21ccaeff8cd686"
+  instance_type = "t2.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.interface.id
+    device_index         = 0
+  }
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_network_interface" "interface" {
+  subnet_id   = aws_subnet.public.id
+
+  tags = {
+    Name = "primary_network_interface"
   }
 }
 
---
+++ network.tf
@@ -0,0 +1,37 @@
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "main-tf"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Public subnet"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_internet_gateway_attachment" "gwattachment" {
+  internet_gateway_id = aws_internet_gateway.gw.id
+  vpc_id              = aws_vpc.vpc.id
+}
+
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/24"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
